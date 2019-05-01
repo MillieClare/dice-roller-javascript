f1ae73c85fe7d086517a6ab19f4f80a92212602c
Millie <millie_clare@outlook.com>
2019-05-01 15:00:02 +0100

    part 2 completed

--- dice-roller.js
+++ dice-roller.js
@@ -1,9 +1,14 @@
+const readlineSync = require('readline-sync');
 function rollDice(){
-    console.log("Welcome! I'll roll a die for you now");
+    console.log("Welcome!");
+    const userRollRequest = readlineSync.question("How many times would you like to roll the dice?")
+    const userRollResults = [];
 
-    let rollResult = Math.ceil(Math.random()*6);
-    console.log(rollResult);
-
+    for(let i = 0; i < userRollRequest; i++){
+    const rollResult = Math.floor(Math.random() * 6) + 1;
+    userRollResults.push(rollResult);
+    }
+    console.log('Your dice rolls are:', userRollResults);
 }
 
 rollDice();
